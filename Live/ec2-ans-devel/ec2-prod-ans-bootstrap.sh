e34f6d442d9389216246623a33dae268e22e0bd0
rb <rambhatat@gmail.com>
2020-06-28 13:12:14 -0700

    File name changes in bootscript

--- Live/ec2-ans-devel/ec2-prod-ans-bootstrap.sh
+++ Live/ec2-ans-devel/ec2-prod-ans-bootstrap.sh
@@ -3,4 +3,4 @@
 
 export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.0"
 aws s3 cp s3://edu.au.cc.ram-image-gallery-config/ec2-prod-ans-latest.sh ./
-/usr/bin/bash ec2-prod-latest.sh
+/usr/bin/bash ec2-prod-ans-latest.sh
